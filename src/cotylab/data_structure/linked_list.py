4222af9c3385605c0e7394e2a4d4ced21ff86f9e
misoncorp <misoncorp@gmail.com>
2022-07-09 17:13:47 +0900

    updated

--- src/cotylab/data_structure/linked_list.py
+++ src/cotylab/data_structure/linked_list.py
@@ -17,44 +17,44 @@ class LinkedList:
     NODE_CLASS = Node
     DELIMITER = ' -> '
 
-    def __init__(self, data=[]):
+    def __init__(self, values=[]):
         self.head = None
         self.tail = None
-        self.current = -1
-        self.set_nodes(data)
+        self.current = None
+        self.insert_nodes(values)
 
-    def set_nodes(self, data):
+    def insert_nodes(self, values):
         self.head = None
         self.tail = None
-        for value in reversed(data):
+        for value in values:
             self.insert_node(value)
+        self.go_to_head()
     
-    def insert_node(self, value, base_node=None, after=True):
+    def insert_node(self, value, after=True):
         node = self.NODE_CLASS(value)
-        if base_node is None:
-            if self.tail is None:
-                self.head = node
-                self.tail = node
-            else:
-                self.tail.next = node
-                self.tail = node
-        elif after:
-            node.next = base_node.next
-            base_node.next = node
+        if self.current is None:
+            self.head = node
+            self.tail = node
+            self.current = node
+            return
+        if after:
+            node.next = self.current.next
+            self.current.next = node
         else:
-            current_node = self.head
-            prev_node = None
-            while current_node is not None:
-                if current_node == base_node:
+            prev = None
+            _node = self.head
+            while _node is not None:
+                if _node == self.current:
                     break
-                prev_node = current_node
-                current_node = current_node.next
-            if prev_node is None:
-                node.next = self.head
+                prev = _node
+                _node = _node.next
+            if prev is None:
                 self.head = node
+                node.next = self.current
             else:
-                node.next = base_node
-                prev_node.next = node
+                prev.next = node
+                node.next = self.current
+        self.current = node
     
     def delete_node(self, node):
         current_node = self.head
@@ -101,34 +101,6 @@ class LinkedList:
 class DoubledLinkedList(LinkedList):
     NODE_CLASS = DoubledNode
     DELIMITER = ' <-> '
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    def insert_node(self, value, base_node=None, after=True):
-        node = self.NODE_CLASS(value)
-        if base_node is None:
-            if self.tail is None:
-                self.head = node
-                self.tail = node
-            else:
-                self.tail.next = node
-                node.prev = self.tail
-                self.tail = node
-        elif after:
-            node.next = base_node.next
-            node.prev = base_node
-            base_node.next = node
-            if node.next is None:
-                self.tail = node
-        else:
-            if base_node.prev is None:
-                node.next = self.head
-                self.head = node
-            else:
-                node.next = base_node
-                node.prev = base_node.prev
-                base_node.prev = node
 
     def prev(self):
         if self.current == -1:
